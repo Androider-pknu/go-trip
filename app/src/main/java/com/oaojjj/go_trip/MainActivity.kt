433d0b4f29b062d795cb8492acc1db7c9924d859
oaojjj <oxojjjj@gmail.com>
2020-10-29 20:09:25 +0900

    init

---
+++ app/src/main/java/com/oaojjj/go_trip/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oaojjj.go_trip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

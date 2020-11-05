d63d7f4082835583601f971e45f4e2c713ce0242
hijongyoon <darkknight35@nate.com>
2020-11-05 19:57:43 +0900

    Feat: Gallery custom

--- app/src/main/java/com/oaojjj/go_trip/Post/MakingPostFragment.kt
+++ app/src/main/java/com/oaojjj/go_trip/Post/MakingPostFragment.kt
@@ -1,74 +1,25 @@
 package com.oaojjj.go_trip.Post
 
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.oaojjj.go_trip.R
 import kotlinx.android.synthetic.main.fragment_make_post.view.*
 
 
 class MakingPostFragment : Fragment() {
-    private val REQ_STORAGE_PERMISSION = 0
-    private val REQ_GALLERY = 1
-    private val FLAG_REQ_STORAGE = 102
-    var rootView : View? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_make_post, container, false)
-        rootView = view
         setPictureButton(view)
         return view
     }
     private fun setPictureButton(view:View){
         view.btn_call_gallery.setOnClickListener {
-            callGallery()
-        }
-    }
-    private fun callGallery(){
-        var writePermission = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        var readPermission = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
-        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE), REQ_STORAGE_PERMISSION)
-        }
-        else{
-            var intent = Intent(Intent.ACTION_PICK)
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            // MediaStore 은 안드로이드에서 외부 저장소를 관리하는 DB. 외부 저장소에 파일을 저장하기 위해서는
-            // MediaStore 을 통해서만 가능.
-            intent.type = "image/*"
-            startActivityForResult(intent, REQ_GALLERY)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            val uri = data?.data
-//            rootView!!.test_img.setImageURI(uri)
-            Log.d("Test",uri.toString())
-            rootView!!.make_post_img_layout.layoutParams.height = (rootView!!.big_layout.height - rootView!!.eText_post.height
-                    - rootView!!.eText_info_location.height - 10)/2
-
-            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,4.toFloat())
-            val picture = ImageView(requireContext())
-            picture.setImageURI(uri)
-            layoutParams.gravity = Gravity.CENTER
-            picture.scaleType = ImageView.ScaleType.CENTER_CROP
-            picture.layoutParams = layoutParams
-            rootView!!.make_post_img_layout.addView(picture)
+            startActivity(Intent(requireContext(),SelectPhoto::class.java))
+            requireActivity().overridePendingTransition(R.anim.up_page,R.anim.holde_page)
         }
     }
 }
--- app/src/main/java/com/oaojjj/go_trip/Post/SelectPhoto.kt
+++ app/src/main/java/com/oaojjj/go_trip/Post/SelectPhoto.kt
@@ -1,39 +1,158 @@
 package com.oaojjj.go_trip.Post
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.oaojjj.go_trip.R
+import kotlinx.android.synthetic.main.activity_select_photo.*
+import kotlinx.android.synthetic.main.select_photo_item.view.*
 import java.util.jar.Manifest
 
 class SelectPhoto : AppCompatActivity() {
-    private val REQ_STORAGE_PERMISSION = 0
-    private val REQ_GALLERY = 1
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_photo)
-        callGallery()
-    }
+    val READ_EXTERNAL_STORAGE_REQUEST = 31
+    private val checkedList = arrayListOf<String>()
+
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.holde_page,R.anim.down_page)
     }
-    private fun callGallery(){
-        var writePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        var readPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE), REQ_STORAGE_PERMISSION)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_photo)
+
+        if (storagePermissionCheck()) {
+            photo_list.adapter = SelectPhotoRecyclerViewAdapter()
+            photo_list.layoutManager = GridLayoutManager(this, 3)
         }
-        else{
-            var intent = Intent(Intent.ACTION_PICK)
-            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            intent.type = "image/*"
-            startActivityForResult(intent, REQ_GALLERY)
+
+        img_btn_cancel.setOnClickListener {
+            onBackPressed()
         }
+
+        btn_upload_picture.setOnClickListener {
+            val intent = Intent()
+            for (s in checkedList) {
+                Log.d(this::class.java.simpleName, "URI: $s")
+            }
+            intent.putStringArrayListExtra("imageURIs", checkedList)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+    private fun storagePermissionCheck(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_EXTERNAL_STORAGE_REQUEST
+            )
+        } else
+            return true
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    photo_list.adapter = SelectPhotoRecyclerViewAdapter()
+                    photo_list.layoutManager = GridLayoutManager(this, 3)
+
+                } else {
+                    Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show()
+        }
+    }
+    inner class SelectPhotoRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        private val uriList = getImageUriList()
+        private val textViewList = mutableListOf<TextView>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.select_photo_item, parent, false)
+            val size = resources.displayMetrics.widthPixels / 3
+            view.layoutParams = ViewGroup.LayoutParams(size, size)
+
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int = uriList.size
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val viewHolder = (holder as CustomViewHolder).itemView
+
+            Glide.with(holder.itemView.context).load(uriList[position])
+                .apply(RequestOptions().centerCrop()).into(viewHolder.imageview_selectphotoitem)
+
+            viewHolder.setOnClickListener {
+                onClick(viewHolder, viewHolder.textview_selectphotoitem_check, position)
+            }
+        }
+        private fun onClick(view: View, textView: TextView, position: Int) {
+            if (uriList[position] !in checkedList && checkedList.size < 4) {
+                checkedList.add(uriList[position])
+                textViewList.add(textView)
+                view.layout_selectphotoitem_check.visibility = FrameLayout.VISIBLE
+                view.textview_selectphotoitem_check.text = (checkedList.lastIndex + 1).toString()
+            } else if (uriList[position] in checkedList) {
+                val removeIndex = checkedList.indexOf(uriList[position])
+                view.layout_selectphotoitem_check.visibility = FrameLayout.INVISIBLE
+                checkedList.removeAt(removeIndex)
+                textViewList.removeAt(removeIndex)
+
+                for ((index, tv) in textViewList.withIndex()) {
+                    tv.text = (index + 1).toString()
+                }
+            }
+        }
+    }
+
+    private fun getImageUriList(): MutableList<String> {
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(MediaStore.Images.ImageColumns.DATA)
+        val cursor = this.contentResolver.query(uri, projection, null, null, null)
+        val uriList = mutableListOf<String>()
+
+        Log.d("MediaStore test", cursor?.columnCount.toString())
+        Log.d("MediaStore test", cursor?.count.toString())
+
+        cursor?.moveToFirst()
+        do {
+            Log.d("MediaStore test", cursor?.position.toString())
+            val path = cursor?.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+//            Log.d("MediaStore test", path!!)
+            uriList.add(path!!)
+        } while (cursor?.moveToNext() == true)
+        cursor.close()
+
+        return uriList
     }
 }
